cf1bb8fedbfb0423369738487f009b8f68f4f898
fredbern <fre19036@byui.edu>
2024-06-15 16:23:30 +1100

    update

--- server.js
+++ server.js
@@ -1,20 +1,8 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const port = process.env.PORT || 3000;
 
-const lesson1Controller = require('./controllers/lesson1');
-
-app.get('/', lesson1Controller.homeRoute);
-
-app.get('/Jana',lesson1Controller.janaRoute);
-
-// app.get('/', (req, res) => {
-//   res.send('Home Page!')
-// });
-
-// app.get('/Jana', (req, res) => {
-//     res.send('Jana Page!')
-//   });
+app.use('/', require('./routes/index.js'));
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`)
